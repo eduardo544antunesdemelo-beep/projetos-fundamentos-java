ca573bfa4d7955d2c61ce75242dd705ada08251e
joaosebben7 <graubr43@gmail.com>
2026-05-24 16:35:16 -0300

    Adiciona calculo da media e resultado final

--- CalculadoraMedia.java
+++ CalculadoraMedia.java
@@ -4,15 +4,33 @@ public class CalculadoraMedia {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite o nome do aluno: ");
-        String nome = sc.nextLine();
+        System.out.println(" \n ==== Calculadora de Média ====  ");
 
-        System.out.print("Digite a primeira nota: ");
+        System.out.print("\nDigite o nome do aluno: ");
+        String nome = sc.nextLine(); 
+
+        System.out.print("\nDigite a primeira nota: ");
         double nota1 = sc.nextDouble();
+        
 
-        System.out.print("Digite a segunda nota: ");
+        System.out.print("\nDigite a segunda nota: ");
         double nota2 = sc.nextDouble();
 
+        System.out.println("\n Digite a terceira nota: ");
+        double nota3 = sc.nextDouble();
+
         sc.close();
-    }
-}
+
+        double media = (nota1 + nota2 + nota3) / 3; 
+          
+        if ( media >= 7) {
+            System.out.println("\nAluno: " + nome + " | Média " + String.format("%.2f", media) + " | Aprovado");
+        } else if (media >= 5) { 
+
+            System.out.println("\nAluno: " + nome + " | Média " + String.format("%.2f", media) + " | Recuperação");
+        } else {
+            System.out.println("\nAluno: " + nome + " | Média " + String.format("%.2f", media) + " | Reprovado");
+        }
+        }
+    
+    } 
